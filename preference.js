b61ab4da7e703857a5cad85c7dec7b764a446b58
Blake <blake@sightsource.net>
2021-08-06 13:30:50 -0400

    added stuff

---
+++ preference.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+let whatAmI = [
+    { flavor: "strawberry", preferenceSequence: 4 },
+    { flavor: "vanilla", preferenceSequence: 2 },
+    { flavor: "pineapple", preferenceSequence: 1 },
+    { flavor: "raspberry", preferenceSequence: 3 }
+];
+
+for (let i = 0; i < whatAmI.length; i++) {
+    if (whatAmI[i].preferenceSequence <= 2) {
+        console.log(`I really like ${whatAmI[i].flavor}!`);
+    } else {
+        console.log(`I do NOT like ${whatAmI[i].flavor} :(`);
+    }
+}
+
+function newPreference(someFlavor, preference) {
+    return { flavor: someFlavor, preferenceSequence: preference };
+}
